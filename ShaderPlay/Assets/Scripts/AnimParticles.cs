ed527f3a4dcbda1fda38d28d5408557009ad30ab
Unknown <jeremy.koskamp@live.nl>
2019-11-27 01:29:54 +0100

    Ready for school

---
+++ ShaderPlay/Assets/Scripts/AnimParticles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimParticles : MonoBehaviour
+{
+    public void SwordGrindParticle()
+    {
+        
+    }
+
+    public void JumpParticle()
+    {
+        
+    }
+
+    public void GroundHitParticle()
+    {
+        
+    }
+}
